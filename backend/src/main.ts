80f3621bbdaf78c1ca870f17dfd37731ea335266
Raul Valdovinos <tu-correo@email.com>
2026-06-01 22:51:50 -0400

    chore: mostrar errores backend en deploy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   );
 
   app.useGlobalInterceptors(new ResponseInterceptor());
-  app.useGlobalFilters(new HttpExceptionFilter());
+  //app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(3000);
 }
